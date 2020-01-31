757accde0d57196ffc1b6979cc223d70998d2f95
Administrator <2971669428@qq.com>
2020-01-31 14:45:01 +0800

    first commit

---
+++ src/GitUseDemo.java
@@ -0,0 +1,12 @@
+/**
+ * @author 侠客
+ * @version: $
+ * @date 2020/1/31 14:35
+ * @Description:
+ */
+public class GitUseDemo {
+
+    public static void main(String[] args) {
+        System.out.println("hello,gitlab:first-commit");
+    }
+}
